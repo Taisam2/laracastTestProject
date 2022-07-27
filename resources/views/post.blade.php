87569f6e6ea3b1f32d833d6e0318210426fc544f
Taisam2 <maschmid2@gmail.com>
2022-07-27 07:39:09 +0200

    initial commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<article>
+    <?= $post; ?>
+</article>
+
+<a href="/">Go Back</a>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1>
+            <a href="posts/my-first-post">My First Post</a>
+        </h1>
+
+        <p>
+
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae et, illo numquam accusantium unde repudiandae, est quod dolorem recusandae, earum facere nam! Odio mollitia quasi voluptatum dignissimos a, eius tempore?
+
+        </p>
+
+    </article>
+
+    <article>
+        <h1>
+            <a href="/posts/my-second-post">My Second Post</a>
+        </h1>
+
+        <p>
+
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae et, illo numquam accusantium unde repudiandae, est quod dolorem recusandae, earum facere nam! Odio mollitia quasi voluptatum dignissimos a, eius tempore?
+
+        </p>
+
+    </article>
+
+    <article>
+        <h1>
+            <a href="/posts/my-third-post">My Third Post</a>
+        </h1>
+
+        <p>
+
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae et, illo numquam accusantium unde repudiandae, est quod dolorem recusandae, earum facere nam! Odio mollitia quasi voluptatum dignissimos a, eius tempore?
+
+        </p>
+
+    </article>
+</body>
