d2fcb969282d8f1501540e20e9afe24dc87f2737
Taisam2 <maschmid2@gmail.com>
2022-08-07 12:13:44 +0200

    Took a few steps in the tutorial. Now starting with Blade intro.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,42 +5,19 @@
 <link rel="stylesheet" href="/app.css">
 
 <body>
-    <article>
-        <h1>
-            <a href="posts/my-first-post">My First Post</a>
-        </h1>
-
-        <p>
-
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae et, illo numquam accusantium unde repudiandae, est quod dolorem recusandae, earum facere nam! Odio mollitia quasi voluptatum dignissimos a, eius tempore?
-
-        </p>
-
-    </article>
+    @foreach ($posts as $post)
 
     <article>
-        <h1>
-            <a href="/posts/my-second-post">My Second Post</a>
-        </h1>
 
-        <p>
-
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae et, illo numquam accusantium unde repudiandae, est quod dolorem recusandae, earum facere nam! Odio mollitia quasi voluptatum dignissimos a, eius tempore?
-
-        </p>
-
+    <h1>
+        <a href="posts/{{ $post->slug }}">
+            {{ $post-> title}}
+        </a>
+    </h1>
+    <div>
+        {{ $post->excerpt }}
+    </div>
+        
     </article>
-
-    <article>
-        <h1>
-            <a href="/posts/my-third-post">My Third Post</a>
-        </h1>
-
-        <p>
-
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae et, illo numquam accusantium unde repudiandae, est quod dolorem recusandae, earum facere nam! Odio mollitia quasi voluptatum dignissimos a, eius tempore?
-
-        </p>
-
-    </article>
+    @endforeach
 </body>
